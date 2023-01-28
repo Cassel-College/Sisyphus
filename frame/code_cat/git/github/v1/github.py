b7a2165ec0f3d4c3f39a3b2d32cf1363cd0251a1
peng liu <pengliu@pengdeMacBook-Pro.local>
2023-01-28 23:24:03 +0800

    Add clone code

--- frame/code_cat/git/github/v1/github.py
+++ frame/code_cat/git/github/v1/github.py
@@ -4,7 +4,7 @@
 from frame.config.v1.load_config import LoadConfig
 from frame.log.log4py import print_log
 from model.code.v1.code import create_code
-from model.code.v1.code_clone import clone_code_core
+from model.code.v1.code_clone import clone_code
 
 
 def clone_code_from_github(info: dict) -> bool:
@@ -14,7 +14,8 @@ def clone_code_from_github(info: dict) -> bool:
     
     local_path = info.get("local_path")
     code = create_code(info=info)
-    if clone_code_core(code=code, local_path=local_path):
+    code.show()
+    if clone_code(code=code, local_path=local_path):
         print_log(log="Clone code success.", level="INFO")
         clone_success = True
     else:
